f80ddc938715cccfa5e61ceb6070cbdfe5b65f8e
clau0310 <clau_itzel93@hotmail.com>
2017-01-16 19:36:58 -0600

    Update Lenguajes.java

--- lenguajes/src/lenguajes/Lenguajes.java
+++ lenguajes/src/lenguajes/Lenguajes.java
@@ -16,7 +16,7 @@ public class Lenguajes {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hola mundo");
+        System.out.println("hola mundo pero desde github");
     }
     
 }
